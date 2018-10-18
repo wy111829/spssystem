c8c9e5af1a17eb8d82eca43b2e9929611a05a065
Aimee1608 <shuigongqian@sina.com>
2018-10-18 23:51:12 +0800

    dealer

--- src/views/systemSet/systemEmailAdressList/index.vue
+++ src/views/systemSet/systemEmailAdressList/index.vue
@@ -5,7 +5,7 @@
                 <el-button slot="append" icon="el-icon-search"></el-button>
             </el-input>
         </div>
-        <el-button type="primary" class="newOrderButton">新建邮箱地址</el-button>
+        <el-button type="primary" class="newOrderButton" @click="dialogFormVisible=true">新建邮箱地址</el-button>
         <el-table :data="tableList" class="table" ref="multipleTable">
             <el-table-column prop="email" label="邮箱"></el-table-column>
             <el-table-column prop="role" label="角色"></el-table-column>
@@ -17,6 +17,29 @@
                 </template>
             </el-table-column>
         </el-table>
+        <el-dialog title="邮箱地址" :visible.sync="dialogFormVisible">
+          <el-form :model="newInfo" label-width="80px">
+            <el-form-item label="邮箱地址">
+              <el-input v-model="newInfo.emailaddress" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="角色">
+              <el-select v-model="newInfo.role" placeholder="请选择活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="状态">
+              <el-radio-group v-model="newInfo.stat">
+                <el-radio label="可用"></el-radio>
+                <el-radio label="禁用"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="text-center">
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+          </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -24,7 +47,7 @@
 export default {
     data() { //选项 / 数据
         return {
-            tableList:[
+            tableList: [
                 {
                     email: "body-paint@list.bmw.com",
                     role: "BMW body paint team",
@@ -35,7 +58,13 @@ export default {
                     role: "BMW Logistics team",
                     state: "Available"
                 },
-            ]
+            ],
+            newInfo: {
+              emailaddress: '',
+              role: '',
+              stat: false
+            },
+            dialogFormVisible: false,
         }
     },
     methods: { //事件处理器
